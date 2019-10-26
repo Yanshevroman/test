7c7d98b7125ffe215a33d06725691510f3bab018
Roman <yanshevroman@gmail.com>
2019-10-27 01:13:22 +0300

    Это первый комит

---
+++ javascript.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+var leftBorderWidth = 1;
+{
+    let second =2;
+}
+const pi= 3.14;
+
+console.log(4/0);
+console.log('string'*9);
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpeg','apple.bmp']
+
+// console.log(arr[0]);
+
+// console.log(4 + "-object");
+
+let incr = 10,
+    decr = 10;
+
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log("2" === 2);
+    let isChecked = false,
+        isClose = false;
+
+ console.log(isChecked || !isClose);
+
